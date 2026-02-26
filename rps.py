e22e6c0ff27ee5110efba9f7c25382069b1a5421
Wric84 <cwright88@live.com>
2026-02-26 18:59:22 -0500

    Updated game

--- rps.py
+++ rps.py
@@ -1,7 +1,45 @@
 # create a rock, paper, scissor game
+import random
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 # 3 options to choose from
 choices = ["rock", "paper", "scissors"]
-# need to be able to take a users input so they can select
-# need to give random choice to computer
-# create what to do depending on different outcomes
+player_score = 0
+computer_score = 0
 
+print("Welcome to Rock, Paper, Scissors")
+while True:
+    # add a score
+    print(f"Your score: {player_score} | Computer: {computer_score} \n")
+    # need to be able to take a users input so they can select
+    player = input("Choose either rock, paper, or scissors (or quit): ").lower()
+
+    if player == "quit":
+        print("Thanks for playing!")
+        print("Final Score")
+        print(f"Your score: {player_score} | Computer: {computer_score} \n")
+        break
+
+    if player not in choices:
+        print("That is not an option. Please choose either rock, paper, or scissors \n")
+        continue
+    # need to give random choice to computer
+    computer = random.choice(choices)
+    print("Your opponent chooses: ", computer)
+    # create what to do depending on different outcomes
+
+    if player == computer:
+        print("Oh - its a tie! \n")
+    elif player == "rock" and computer == "scissors":
+        print(Fore.GREEN + "You win rockstar! \n")
+        player_score +=1
+    elif player == "scissors" and computer == "paper":
+        print(Fore.GREEN + "You are really cutting down your opponents! You Win!!! \n")
+        player_score += 1
+    elif player == "paper" and computer == "rock":
+        print(Fore.GREEN + "You win!!! \n")
+        player_score +=1
+    else:
+        print("haha - you lose! Better luck next time! \n")
+        computer_score +=1
